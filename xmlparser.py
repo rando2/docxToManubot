379bf677aba0c4c516d694e146330193fc59e1c5
HM Rando <halie.rando@pennmedicine.upenn.edu>
2022-06-14 11:43:56 -0400

    generates python-track-changes, start coding edits

--- xmlparser.py
+++ xmlparser.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import docx #may be able to use normal version?
+import pysbd
 
 def readDocx(filename):
     """Read the paragraphs of the docx file in as a list.
@@ -21,42 +22,67 @@ def readDocx(filename):
         paragraphs.append(para.text)
     return(paragraphs)
 
+def readMarkdown():
+    with open("origmdMD.md", 'r') as origFile:
+        # Read & format original markdown
+        origTextRaw = origFile.read()
+        origText = origTextRaw.splitlines()
+        origText = [t.strip() for t in origText]
+        #print(origText)
+
 def findChanges(xmlFile):
-    """Inspired by N. Fisher: https://stackoverflow.com/questions/61316140/extracting-data-from-xml-of-docx-document"""
     tree = ET.parse(xmlFile)
     root = tree.getroot()
+    nsmap = {'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
 
-    # Declare namespace dictionary
-    nsmap = {'w':'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
+    # Iterate through body, storing original text as it is encountered
+    # Each block of text as rendered in the XML file is stored as an item in a list
+    # Insertions are marked with *** and deletions are marked with ~~~
+    # This essentially renders the docx text into Python-readable track changes
+    textblocks = []
+    for para in root[0].findall('.//w:p', nsmap): #each paragraph
+        for section in para:
+            if len(section.attrib) == 0:
+                for textBlock in section.findall('.//w:t', nsmap):
+                    textblocks.append(textBlock.text)
+            else: # these are the insertions + some random metadata changes
+                for data in section.findall('.//w:r//w:t', nsmap):
+                    insertedText = data.text
+                    textblocks.append("***"+insertedText+"***")
+                for data in section.findall('.//w:r//w:delText', nsmap):
+                    deletedText = data.text
+                    textblocks.append("~~~"+deletedText+"~~~")
+    return textblocks
 
-    # Iterate through body
-    for child in root[0]:
-        insertions = child.findall('.//w:ins', nsmap)
-        if len(insertions) > 0:
-            for i in insertions:
-                for j in i.findall('.//w:t', nsmap):
-                    continue
-                    print(j.text, j.tag)
-        else:
-            print(child.attrib)
-            continue
-            for i in child.findall('.//w:r//w:t', nsmap):
-                print(i.text)
-                continue
-                if len(i.text) > 0:
-                    print(i.text)
-                else:
-                    print('except')
-                    print(i.findall('.//w:t', nsmap))
+def insert(textblocks, i, markdown):
+    
 
+def replaceLines(textblocks, markdown):
+    """Use the mark-up in the textblocks list to determine whether we are deleting, inserting
+    or replacing text."""
+    i = 0
+    while i < len(textblocks):
+        if textblocks[i][:3] == "~~~":
+            if textblocks[i+1][:3] == "***":
+                print("replace", textblocks[i:i+2])
+            else:
+                print("delete", textblocks[i])
+        elif textblocks[i][:3] == "***":
+            if textblocks[i+1][:3] == "~~~":
+                print("replace", textblocks[i:i+2])
+            else:
+                print("insert", textblocks[i])
+        i +=1
 
-    """"#loop through all w:t tags and append their values to list
-    for i in root.findall('.//w:r//w:t', nsmap):
-        for j in root.findall('.//w:ins', nsmap):
-        for j in i.findall('.//w:t', nsmap):
-            print(j.text)
-        #print(i.attrib, i.text, i.tag, i.text)
-    """
+    # insertion on its own
+    # insertion next to a deletion
 
-readDocx("test.docx")
-findChanges("./word/document.xml")
+def main():
+    # Pull the markdown from master
+    markdown = readMarkdown()
+
+    # Identify which text has stayed the same and which has changed
+    text = findChanges("./word/document.xml")
+    replaceLines(text, markdown)
+
+main()
