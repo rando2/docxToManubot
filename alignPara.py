41601254f3c1c9e6ef957fda95060e892ce123ba
HM Rando <halie.rando@pennmedicine.upenn.edu>
2022-07-08 12:23:52 -0400

    last commit with failed attempts included in alignPara

--- alignPara.py
+++ alignPara.py
@@ -3,11 +3,14 @@ import numpy as np
 from plotnine import *
 from scipy.stats import zscore
 from sklearn.linear_model import LinearRegression
+from plydata import tidy
+from plydata import *
 
-def calcReg(max_pos):
+def calcReg(x, y):
     # Draw an identity line capturing max score indices based on row number
-    model = LinearRegression().fit(pd.DataFrame(max_pos.index), pd.DataFrame(max_pos["max_col"]))
-    return model.predict(pd.DataFrame(max_pos.index))
+    data = pd.DataFrame(data=y, index=x).dropna()
+    model = LinearRegression().fit(pd.DataFrame(data.index), pd.DataFrame(data["y"]))
+    return model.predict(pd.DataFrame(x))
 
 def checkModel(df, groupNum = 0):
     match = list()
@@ -47,12 +50,109 @@ def applyChanges(match, unmatch, max_pos):
 heatmap = pd.read_csv("heatmap.csv", index_col=None)
 
 # Identify the best match for each row
-max_pos = pd.DataFrame({'max_col': pd.to_numeric(heatmap.T.idxmax(), errors='ignore'),
-                       'max_val': pd.to_numeric(heatmap.T.max(), errors='ignore')})
+max_pos = pd.DataFrame({'max_col': pd.to_numeric(heatmap.T.idxmax(), errors='ignore')})
+                       #'max_val': pd.to_numeric(heatmap.T.max(), errors='ignore')})
 max_pos.index = pd.to_numeric(max_pos.index, errors='ignore')
 
-# Predict the cells that will hold the best matches using linear regression
-max_pos["prediction"] = calcReg(max_pos)
+# Clean up outliers & non-monotonic changes (max_col is always first max, so should be biased
+# towards lower numbers)
+# Then predict the where best match will fall using linear regression, then identify places with exact or
+# very close matches
+max_pos = max_pos \
+          >> define(row = max_pos.index.astype("int"), diff='max_col - max_col.shift(1)')\
+          >> define(z='zscore(diff, nan_policy="omit")')\
+          >> define(y=if_else('(diff < 0) | (z >= 2)', 'np.nan', 'max_col'))\
+          >> define(prediction='calcReg(row, y)')\
+          >> define(match=if_else('y == np.floor(prediction)', 'np.floor(prediction)',
+                                      if_else('y == np.ceil(prediction)', 'np.ceil(prediction)', -1)))\
+          >> select("row", "max_col", "y", "prediction", "match")
+
+# Manually set first and last to ensure all data is included
+max_pos.at[0, "match"] = 0
+max_pos.at[len(max_pos.index)-1, "match"] = len(heatmap.columns)
+
+# Iteratively update the matches where monotonic, nearly-linear increase is detected
+update = max_pos >> query('match == -1') >> count()
+prev = max_pos >> count()
+prev = prev.values[0][0]
+while update.values[0][0] < prev :
+    prev = update.values[0][0]
+    max_pos = max_pos \
+              >> define(match=if_else('(match == -1) & (match.shift(1) + 1 == max_col)',
+                                      'match.shift(1) + 1', 'match')) \
+              >> define(match=if_else('(match == -1) & (match.shift(-1) - 1 == max_col)',
+                                      'match.shift(-1) - 1', 'match'))\
+              >> define(match=if_else(('(match == -1) & (match.shift(1) != -1) & (match.shift(1) + 1 == max_col)'),
+                                      'match.shift(1) + 1',
+                                      'match'))\
+              >> define(match=if_else(('(match == -1) & (match.shift(1) != -1) & (match.shift(-1) != -1) &'
+                                      '(match.shift(1) + 2 == match.shift(-1))'),
+                                      'match.shift(1) + 1',
+                                      'match')) \
+              >> define(match=if_else(('(match == -1) & (match.shift(1) != -1) & ((match.shift(1) == match.shift(-1))'
+                                       '| (match.shift(1) + 1 == match.shift(-1)))'),
+                                      'match.shift(1)',
+                                      'match')) \
+              >> define(prediction2=if_else('(match == -1) & (match.shift(1) != -1)',
+                                          'prediction + (match.shift(1) - prediction.shift(1))',
+                                          'prediction'))\
+              >> define(match=if_else('(match == -1)',
+                                         if_else('max_col == np.floor(prediction2)',
+                                                 'np.floor(prediction2)',
+                                                 if_else('max_col == np.ceil(prediction2)',
+                                                         'np.ceil(prediction2)',
+                                                         -1)),
+                                         'match')) \
+              >> define(match=if_else('(match != -1) & (match < match.shift(1))',
+                                      -1, 'match')) # correct any that are not monotonic
+
+    update = max_pos \
+             >> query('match == -1')\
+             >> count()
+
+# Handle remaining blank predictions
+undefined = max_pos \
+            >> define(last= 'match.shift(1)', next= 'match.shift(-1)')\
+            >> query("match == -1")\
+            >> select("row", "max_col", "prediction", "y", "last", "next")
+
+if len(undefined.index) > 0:
+    print(undefined)
+    exit("missing some assignments")
+
+"""
+# Identify sections where prediction fails
+runs = [[]]
+for rowNum in range(0, len(undefined.index)):
+    row = int(list(undefined.iloc[rowNum].values)[0])
+    if len(runs[-1]) == 0:
+        runs[-1].append(row)
+    elif row == runs[-1][-1] + 1:
+        runs[-1].append(row)
+    else:
+        runs.append([row])
+
+for run in runs:
+    chunk = max_pos.iloc[run[0]-1:run[-1] + 2]
+    print(calcReg(chunk))
+    #new_model_x = pd.Series()
+"""
+
+
+#print(max_pos >> define(match=if_else('(match != -1) & ( (match.shift(1) > match) |  (match.shift(-1) < match)) ',
+#                        -1, 'match')))
+#for row_num in range(0,len(max_pos)):
+#    max_col, max_val, pred, match, diff = max_pos.iloc[[row_num]].to_numpy()[0]
+#    print(max_col, match)
+#    #print(max_col, max_val, pred_low, pred_high, match)
+    #for value in [pred_low, pred_high]:
+    #    if max_col == value:
+    #        print("match")
+    #        max_pos.at[i, "match"] = value
+
+
+max_pos.to_csv("tmp/max.csv")
+exit()
 
 # Evaluate predictions against actual max values
 max_pos["match"], max_pos["unmatched"], groupNum = checkModel(max_pos)
@@ -98,7 +198,7 @@ print(max_pos)
     #elif len(group.index) == 0:
     #    continue
 
-max_pos.to_csv("tmp/max.csv")
+
 exit(0)
 
 """
